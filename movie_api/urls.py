b2f28435742176c79a36caa193a8b20154060192
dakixr <dakixr@gmail.com>
2024-06-06 19:27:43 +0200

    add urls

--- movie_api/urls.py
+++ movie_api/urls.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
+from . import views
 
 urlpatterns = [
-    path("", lambda _: HttpResponse("OK"), name="root")
+    path("", lambda _: HttpResponse("App running correctly!"), name="root"),
+    path("draw-chart/", views.graph_endpoint, name="draw_chart"),
+    path("load-data/", views.load_data_endpoint, name="load_data"),
+    path("export-data/", views.export_data, name="export_data"),
 ]
-
