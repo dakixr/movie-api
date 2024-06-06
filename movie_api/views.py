07f1bcb4d09b4b2429eec3b3cee976d2e7b596e8
dakixr <dakixr@gmail.com>
2024-06-06 21:06:18 +0200

    re-add try except block

--- movie_api/views.py
+++ movie_api/views.py
@@ -38,71 +38,71 @@ def load_data_endpoint(request):
         file_name = uploaded_file.name
         file_path = default_storage.save(uploaded_file.name, uploaded_file)
 
-        # try:
-        if file_name.endswith(".csv"):
-            data = pd.read_csv(default_storage.open(file_path))
-        elif file_name.endswith(".xlsx"):
-            data = pd.read_excel(default_storage.open(file_path))
-        else:
-            return HttpResponse(f"Unsupported file format: {file_name}", status=400)
-        # Parsing and saving data to the database
-        for _, row in data.iterrows():
+        try:
+            if file_name.endswith(".csv"):
+                data = pd.read_csv(default_storage.open(file_path))
+            elif file_name.endswith(".xlsx"):
+                data = pd.read_excel(default_storage.open(file_path))
+            else:
+                return HttpResponse(f"Unsupported file format: {file_name}", status=400)
+            # Parsing and saving data to the database
+            for _, row in data.iterrows():
 
-            # Fetch or create the movie without setting the genres yet
-            rating = pd.to_numeric(row["avg_vote"], errors="coerce") # Transform to None if value is non-numeric
-            rating = rating if pd.notna(rating) else None
-            movie, created = Movie.objects.get_or_create(
-                Title=row["title"],
-                Rating=rating , 
-                ReleaseYear=row["year"],
-            )
+                # Fetch or create the movie without setting the genres yet
+                rating = pd.to_numeric(row["avg_vote"], errors="coerce") # Transform to None if value is non-numeric
+                rating = rating if pd.notna(rating) else None
+                movie, created = Movie.objects.get_or_create(
+                    Title=row["title"],
+                    Rating=rating , 
+                    ReleaseYear=row["year"],
+                )
 
-            # Parsing and adding the directors
-            if isinstance(row["director"], str):
-                directors_names = [
-                    director.strip() for director in row["director"].split(",")
-                ]
+                # Parsing and adding the directors
+                if isinstance(row["director"], str):
+                    directors_names = [
+                        director.strip() for director in row["director"].split(",")
+                    ]
 
-                for director_name in directors_names:
-                    director, created = Director.objects.get_or_create(
-                        Name=director_name,
-                        defaults={
-                            "BirthYear": None,  # Assuming BirthYear is not available in the data
-                            "Nationality": None,  # Assuming Nationality is not available in the data
-                        },
-                    )
-                    movie.Directors.add(director)
+                    for director_name in directors_names:
+                        director, created = Director.objects.get_or_create(
+                            Name=director_name,
+                            defaults={
+                                "BirthYear": None,  # Assuming BirthYear is not available in the data
+                                "Nationality": None,  # Assuming Nationality is not available in the data
+                            },
+                        )
+                        movie.Directors.add(director)
 
-            # Adding the genres
-            if isinstance(row["genre"], str):
-                # Split the genres by comma and strip any extra whitespace
-                genres_list = [genre.strip() for genre in row["genre"].split(",")]
+                # Adding the genres
+                if isinstance(row["genre"], str):
+                    # Split the genres by comma and strip any extra whitespace
+                    genres_list = [genre.strip() for genre in row["genre"].split(",")]
 
-                # Clear existing genres if the movie was already present
-                if not created:
-                    movie.Genres.clear()
+                    # Clear existing genres if the movie was already present
+                    if not created:
+                        movie.Genres.clear()
 
-                # Fetch or create each genre and add it to the movie
-                for genre_name in genres_list:
-                    genre, _ = Genre.objects.get_or_create(Name=genre_name)
-                    movie.Genres.add(genre)
+                    # Fetch or create each genre and add it to the movie
+                    for genre_name in genres_list:
+                        genre, _ = Genre.objects.get_or_create(Name=genre_name)
+                        movie.Genres.add(genre)
 
-            if isinstance(row["actors"], str):
-                actors_list = row["actors"].split(", ")
-                for actor_name in actors_list:
-                    actor, created = Actor.objects.get_or_create(
-                        Name=actor_name,
-                        defaults={
-                            "BirthYear": None,  # Assuming BirthYear is not available in the data
-                            "Nationality": None,  # Assuming Nationality is not available in the data
-                        },
-                    )
-                    MovieActor.objects.get_or_create(MovieID=movie, ActorID=actor)
+                if isinstance(row["actors"], str):
+                    actors_list = row["actors"].split(", ")
+                    for actor_name in actors_list:
+                        actor, created = Actor.objects.get_or_create(
+                            Name=actor_name,
+                            defaults={
+                                "BirthYear": None,  # Assuming BirthYear is not available in the data
+                                "Nationality": None,  # Assuming Nationality is not available in the data
+                            },
+                        )
+                        MovieActor.objects.get_or_create(MovieID=movie, ActorID=actor)
 
-        return HttpResponse("Data loaded successfully", status=201)
+            return HttpResponse("Data loaded successfully", status=201)
 
-        # except Exception as e:
-        #     return HttpResponse(f"Error when uploading the file: {e}", status=500)
+        except Exception as e:
+            return HttpResponse(f"Error when uploading the file: {e}", status=500)
 
     return HttpResponse("Invalid HTTP Method", status=400)
 
