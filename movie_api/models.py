013bb77db00694fc5b25f2deb7bca0b78e8533c2
dakixr <dakixr@gmail.com>
2024-06-06 17:37:43 +0200

    create models and initial migration

--- movie_api/models.py
+++ movie_api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+# Notes:
+
+# Using UUIDs to not expose to the client the number of entries in each table
+# the default id in django in an autoincrement BigInt 
+
+# I am using camel case to fulfill the contract of the DB names
+# In a normal project i would use snake-case for table columns names
+
+class Movie(models.Model):
+    MovieID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Title = models.CharField(max_length=255)
+    ReleaseYear = models.IntegerField()
+    Director = models.ForeignKey('Director', on_delete=models.CASCADE)
+    Genre = models.CharField(max_length=100)
+    Rating = models.DecimalField(max_digits=3, decimal_places=1)
+
+    def __str__(self):
+        return self.Title
+
+class Actor(models.Model):
+    ActorID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Name = models.CharField(max_length=255)
+    BirthYear = models.IntegerField()
+    Nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.Name} {self.BirthYear}"
+
+class Director(models.Model):
+    DirectorID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    Name = models.CharField(max_length=255)
+    BirthYear = models.IntegerField()
+    Nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Name
+
+class MovieActor(models.Model):
+    MovieID = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    ActorID = models.ForeignKey(Actor, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('MovieID', 'ActorID')
